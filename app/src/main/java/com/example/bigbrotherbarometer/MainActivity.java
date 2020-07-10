ec0a70c8749e2e60e707144aee866cf5aece3f5c
Rishabh Verma <rishabhverma.rv@gmail.com>
2020-07-10 15:18:21 -0700

    Begin implementing barometer display.

--- app/src/main/java/com/example/bigbrotherbarometer/MainActivity.java
+++ app/src/main/java/com/example/bigbrotherbarometer/MainActivity.java
@@ -1,3 +1,12 @@
+/*
+
+Gotta change one line at a time to implement multiple sensors.
+Can one SensorEventListener be registered with multiple sensors?
+I think so, because that's how the accelerometer sensors work;
+there are three distinct sensors, so I should be able to add a fourth easily
+by appending to the sensor list... right?
+ */
+
 package com.example.bigbrotherbarometer;
 
 import android.os.Bundle;
@@ -9,6 +18,7 @@ import android.hardware.SensorEvent;
 import android.hardware.Sensor;
 
 import java.util.List;
+import java.util.Arrays;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,15 +28,18 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
     SensorManager sm = null;
     TextView textView1 = null;
-    List sensors;  // TODO rename
+    List<Sensor> sensors;
 
     SensorEventListener sel = new SensorEventListener() {
         public void onAccuracyChanged(Sensor sensor, int accuracy) {}
         public void onSensorChanged(SensorEvent event) {
             float[] values = event.values;
-            String display = "x: " + values[0] + "\n"
+            String display = Arrays.toString(values);
+            /*String display = "x: " + values[0] + "\n"
                            + "y: " + values[1] + "\n"
-                           + "z: " + values[2];
+                           + "z: " + values[2] + "\n"
+                           + "p: " + values[3];
+            */
             textView1.setText(display);
         }
     };
@@ -43,11 +56,35 @@ public class MainActivity extends AppCompatActivity {
         textView1 = (TextView) findViewById(R.id.textView1);
 
         sensors = sm.getSensorList(Sensor.TYPE_ACCELEROMETER);
-        if (sensors.size() > 0) {
-            sm.registerListener(sel, (Sensor) sensors.get(0), SensorManager.SENSOR_DELAY_NORMAL);
-        } else {
-            Toast.makeText(getBaseContext(), "Error: No Accelerometer.", Toast.LENGTH_LONG).show();
+
+
+
+        sm.registerListener(sel, (Sensor) sensors.get(0), SensorManager.SENSOR_DELAY_NORMAL);
+
+        /*
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // Get a SensorManager instance
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        textView1 = (TextView) findViewById(R.id.textView1);
+
+        sensors.addAll(sm.getSensorList(Sensor.TYPE_ACCELEROMETER));
+        sensors.addAll(sm.getSensorList(Sensor.TYPE_PRESSURE));
+        sensors.addAll(sm.getSensorList(Sensor.TYPE_GYROSCOPE));
+
+
+
+
+        for (int i = 0; i < sensors.size(); i++) {
+            sm.registerListener(sel, (Sensor) sensors.get(i), SensorManager.SENSOR_DELAY_NORMAL);
         }
+        // The above block assumes all sensors are present. Otherwise, the indices of the
+        // SensorEvent values will be misaligned.
+
+         */
     }
 
     @Override
